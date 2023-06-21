a6da33273277a1f5e78991c55827b033e1c8ffae
Sam Mercer <smercer10@proton.me>
2023-06-21 15:22:30 +0100

    refactor: modify bits in-place
    Pass a bitboard reference to SetBit and ClearBit functions so the
    bitboard is modified in-place.

--- engine/bitboard/bitboard_test.go
+++ engine/bitboard/bitboard_test.go
@@ -1,8 +1,8 @@
-// Package b provides bitboard utilities.
-package b
+// Package bitboard provides bitboard utilities.
+package bitboard
 
 import (
-	c "gogambit/engine/const"
+	c "gogambit/engine/constants"
 	"testing"
 )
 
@@ -10,11 +10,15 @@ import (
 func TestSetBit(t *testing.T) {
 	var bb Bitboard = 0x0
 
-	if bb = SetBit(bb, c.A1); bb != 0x1 {
+	SetBit(&bb, c.A1)
+
+	if bb != 0x1 {
 		t.Errorf("SetBit failed: expected 0x1, got 0x%x", bb)
 	}
 
-	if bb = SetBit(bb, c.H8); bb != 0x8000000000000001 {
+	SetBit(&bb, c.H8)
+
+	if bb != 0x8000000000000001 {
 		t.Errorf("SetBit failed: expected 0x8000000000000001, got 0x%x", bb)
 	}
 }
@@ -23,11 +27,15 @@ func TestSetBit(t *testing.T) {
 func TestClearBit(t *testing.T) {
 	var bb Bitboard = 0x8000000000000001
 
-	if bb = ClearBit(bb, c.A1); bb != 0x8000000000000000 {
+	ClearBit(&bb, c.A1)
+
+	if bb != 0x8000000000000000 {
 		t.Errorf("ClearBit failed: expected 0x8000000000000000, got 0x%x", bb)
 	}
 
-	if bb = ClearBit(bb, c.H8); bb != 0x0 {
+	ClearBit(&bb, c.H8)
+
+	if bb != 0x0 {
 		t.Errorf("ClearBit failed: expected 0x0, got 0x%x", bb)
 	}
 }
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	b "gogambit/engine/bitboard"
-	c "gogambit/engine/const"
+	c "gogambit/engine/constants"
 )
 
 func main() {
-	var bb b.Bitboard = 0x0
+	var bb b.Bitboard = 0x8000000000000001
 
-	bb = b.SetBit(bb, c.D4)
+	b.ClearBit(&bb, c.A1)
+	b.ClearBit(&bb, c.H8)
 
-	b.Print(bb)
+	b.PrintBitboard(bb)
 }

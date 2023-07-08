dd4e5d66d19a3eb6407c318490982503faa6798e
Sam Mercer <smercer10@proton.me>
2023-07-08 19:29:52 +0100

    feat: get slider attacks
    Initialize LUTs for bishop/rook attacks and relevant occupancy masks.
    Create and test functions to get bishop and rook attacks for a given
    square and board occupancy state.

--- engine/attacks/rook_test.go
+++ engine/attacks/rook_test.go
@@ -7,6 +7,31 @@ import (
 	"testing"
 )
 
+// TestGetRookAttacks tests the GetRookAttacks function.
+func TestGetRookAttacks(t *testing.T) {
+	testCases := []struct {
+		sq        int
+		occupancy Bitboard
+		expect    Bitboard
+	}{
+		{D4, 0x82080000, 0x8080808f6080000},
+		{E3, 0x801000080001, 0x1010e81010},
+		{H8, 0x4000800000000000, 0x4080800000000000},
+		{C3, 0x0, 0x404040404fb0404},
+		{A1, 0x100800000200, 0x1010101010101fe},
+		{B6, 0x2100000000200, 0x21d0202020200},
+	}
+
+	InitSliderAttacks(Rook)
+
+	for _, tc := range testCases {
+		if result := GetRookAttacks(tc.sq, tc.occupancy); result != tc.expect {
+			t.Errorf("GetRookAttacks failed for sq = %d, occupancy = 0x%x: expect 0x%x, got 0x%x",
+				tc.sq, tc.occupancy, tc.expect, result)
+		}
+	}
+}
+
 // TestMaskRelevantRookOccupancy tests the MaskRelevantRookOccupancy function.
 func TestMaskRelevantRookOccupancy(t *testing.T) {
 	testCases := []struct {
@@ -30,9 +55,9 @@ func TestMaskRelevantRookOccupancy(t *testing.T) {
 // TestGenRookAttacksOnTheFly tests the GenRookAttacksOnTheFly function.
 func TestGenRookAttacksOnTheFly(t *testing.T) {
 	testCases := []struct {
-		sq       int
-		blockers Bitboard
-		expect   Bitboard
+		sq        int
+		occupancy Bitboard
+		expect    Bitboard
 	}{
 		{D4, 0x822000000, 0x836080808},
 		{E3, 0x10000000880000, 0x10101010e81010},
@@ -41,9 +66,9 @@ func TestGenRookAttacksOnTheFly(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		if result := GenRookAttacksOnTheFly(tc.sq, tc.blockers); result != tc.expect {
-			t.Errorf("GenRookAttacksOnTheFly failed for sq = %d, blockers = 0x%x: expect 0x%x, got 0x%x",
-				tc.sq, tc.blockers, tc.expect, result)
+		if result := GenRookAttacksOnTheFly(tc.sq, tc.occupancy); result != tc.expect {
+			t.Errorf("GenRookAttacksOnTheFly failed for sq = %d, occupancy = 0x%x: expect 0x%x, got 0x%x",
+				tc.sq, tc.occupancy, tc.expect, result)
 		}
 	}
 }
